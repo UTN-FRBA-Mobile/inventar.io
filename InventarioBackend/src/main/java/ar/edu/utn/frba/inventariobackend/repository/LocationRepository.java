a09a012e5602df6db23c8050133d5e5a2a88dc93
Fernando Martín Besteiro <fmbesteiro@gmail.com>
2025-05-08 15:51:15 -0300

    Agrego javadoc pendiente

--- InventarioBackend/src/main/java/ar/edu/utn/frba/inventariobackend/repository/LocationRepository.java
+++ InventarioBackend/src/main/java/ar/edu/utn/frba/inventariobackend/repository/LocationRepository.java
@@ -4,6 +4,13 @@ import ar.edu.utn.frba.inventariobackend.model.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository interface for performing CRUD operations on {@link Location} entities.
+ * <p>
+ * Extends {@link JpaRepository} to inherit methods for saving, deleting,
+ * and finding {@link Location} entities by their ID or other criteria.
+ * </p>
+ */
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
 }
