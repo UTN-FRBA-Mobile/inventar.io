a69109016885d73e6d073fe35d1d302bf7ac56ce
Lucas Borrazás <lucas.borrazas@gmail.com>
2025-05-23 21:09:03 -0300

    Creación BranchLocationBar

---
+++ InventarioApp/app/src/main/java/ar/edu/utn/frba/inventario/components/BranchLocationBar.kt
@@ -0,0 +1,52 @@
+package ar.edu.utn.frba.inventario.components
+
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.LocationOn
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import ar.edu.utn.frba.inventario.R
+
+@Composable
+fun BranchLocationBar(
+    branchName: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .padding(horizontal = 16.dp, vertical = 2.dp)
+            .background(
+                color = MaterialTheme.colorScheme.secondaryContainer ,
+                shape = MaterialTheme.shapes.small
+            ),
+        verticalAlignment = Alignment.CenterVertically
+
+    ) {
+        Icon(
+            imageVector = Icons.Outlined.LocationOn,
+            contentDescription = "Ubicación sucursal",
+            tint = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+
+        Text(
+            text = stringResource(R.string.branch_name, branchName),
+            style = MaterialTheme.typography.titleSmall,
+            fontWeight = FontWeight.SemiBold,
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
+}
