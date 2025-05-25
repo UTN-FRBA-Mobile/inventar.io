cc493e5c012aa2a1b7ca2ff5061b105aa20f1484
GustavoChoque <e_gustavo14@hotmail.com>
2025-05-25 14:08:49 -0300

    Se agrega botones de Scan y Siguiente en la pantalla de los detalles de un envio

--- InventarioApp/app/src/main/java/ar/edu/utn/frba/inventario/screens/ShipmentScreen.kt
+++ InventarioApp/app/src/main/java/ar/edu/utn/frba/inventario/screens/ShipmentScreen.kt
@@ -2,9 +2,11 @@ package ar.edu.utn.frba.inventario.screens
 
 import android.annotation.SuppressLint
 import android.text.Layout
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +17,8 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
@@ -41,20 +45,21 @@ import ar.edu.utn.frba.inventario.viewmodels.Product
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun ShipmentScreen(viewModel:ShipmentViewModel = hiltViewModel(), navController: NavController, id:Int){
-    Scaffold{
-
-        ShipmentBodyContent(viewModel, id)
+    Scaffold(bottomBar = {ButtonBox()}){
+        innerPadding ->
+        ShipmentBodyContent(viewModel, id,innerPadding)
     }
 
 }
 @Composable
-fun ShipmentBodyContent(viewModel:ShipmentViewModel, id:Int){
+fun ShipmentBodyContent(viewModel:ShipmentViewModel, id:Int, innerPadding: PaddingValues){
     viewModel.loadShipment(id)
     val shipment by viewModel.shipment.collectAsState()
 
 
     Column(modifier = Modifier
         .fillMaxSize()
+        .padding(innerPadding)
         ) {
         Spacer(modifier = Modifier
             .height(50.dp))
@@ -69,7 +74,7 @@ fun ShipmentBodyContent(viewModel:ShipmentViewModel, id:Int){
                     fontWeight = FontWeight.Bold
                 )
                 Text(text = "Comprador: ${shipment.customerName}", fontWeight = FontWeight.SemiBold)
-                Text(text = "Total de productos: ${shipment.products.size}", fontWeight = FontWeight.SemiBold)
+                Text(text = "Total de productos unicos: ${shipment.products.size}", fontWeight = FontWeight.SemiBold)
             }
         }
         LazyColumn(modifier = Modifier
@@ -82,16 +87,6 @@ fun ShipmentBodyContent(viewModel:ShipmentViewModel, id:Int){
         }
         Spacer(modifier = Modifier
             .height(10.dp))
-
-        Row {
-            Button(onClick = {}){
-                Text("Siguiente")
-            }
-            Button(onClick = {}){
-                Text("Scan")
-            }
-        }
-
 
     }
 }
@@ -107,17 +102,48 @@ fun ProductItem(viewModel:ShipmentViewModel,product:Product){
             Spacer(modifier = Modifier
                 .height(10.dp))
             Row {
-                Text("x${product.quantity}")
+                Text("${product.quantity} Requeridos")
                 Spacer(modifier = Modifier.width(25.dp)
                     .weight(2f))
                 Box(contentAlignment = Alignment.CenterEnd,
                     modifier = Modifier
                         .background(color = Color.White)){
-                    Text(text="Cargado: ${viewModel.getLoadedQuantityProduct(product.id)}")
+                    Text(text="${viewModel.getLoadedQuantityProduct(product.id)} Cargados")
                 }
             }
         }
     }
+}
+
+@Composable
+fun ButtonBox(){
+    Column {
+        Box(contentAlignment = Alignment.Center, modifier = Modifier
+            .fillMaxWidth()
+            .height(70.dp)
+            .padding(10.dp)){
+            Row {
+
+                Button(colors = ButtonDefaults.buttonColors(Color(0xFFE7ECFB)),
+                    border = BorderStroke(1.dp, color = Color.Gray),onClick = {}, modifier = Modifier
+                        .fillMaxSize()
+                        .weight(1f)){
+                    Text(text = "Siguiente", color = Color.Black, fontSize = 25.sp,
+                        fontWeight = FontWeight.Bold)
+                }
+                Button(onClick = {}, modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f)
+                ){
+                    Text(text = "Scan", color = Color.Black, fontSize = 25.sp,
+                        fontWeight = FontWeight.Bold)
+                }
+            }
+        }
+        Spacer(modifier = Modifier
+            .height(20.dp))
+    }
+
 }
 
 @Preview
@@ -145,4 +171,6 @@ fun vistaProd(){
             }
         }
     }
+    ButtonBox()
 }
+
