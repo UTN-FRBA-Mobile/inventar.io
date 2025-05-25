caf28abd63bb9187a0394526cd04f10f7ec2dc4f
GustavoChoque <e_gustavo14@hotmail.com>
2025-05-25 01:04:56 -0300

    Se acomoda la estructura de la vista de envio, tanto en la cabecera del envio como en la lista de productos

--- InventarioApp/app/src/main/java/ar/edu/utn/frba/inventario/screens/ShipmentScreen.kt
+++ InventarioApp/app/src/main/java/ar/edu/utn/frba/inventario/screens/ShipmentScreen.kt
@@ -1,15 +1,22 @@
 package ar.edu.utn.frba.inventario.screens
 
 import android.annotation.SuppressLint
+import android.text.Layout
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -20,16 +27,22 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import ar.edu.utn.frba.inventario.viewmodels.ShipmentViewModel
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ar.edu.utn.frba.inventario.viewmodels.Product
+
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun ShipmentScreen(viewModel:ShipmentViewModel = hiltViewModel(), navController: NavController, id:Int){
     Scaffold{
+
         ShipmentBodyContent(viewModel, id)
     }
 
@@ -39,44 +52,97 @@ fun ShipmentBodyContent(viewModel:ShipmentViewModel, id:Int){
     viewModel.loadShipment(id)
     val shipment by viewModel.shipment.collectAsState()
 
+
     Column(modifier = Modifier
         .fillMaxSize()
-        .padding(20.dp)) {
-        Text(
-            text = "Envio ${shipment.number}",
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold
-        )
-        Text(text = "Comprador: ${shipment.customerName}")
-        Text(text = "Total de productos: ${shipment.products.size}")
+        ) {
+        Spacer(modifier = Modifier
+            .height(50.dp))
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .background(color = Color(0xFFE7ECFB))){
+            Column (modifier = Modifier
+                .padding(20.dp)){
+                Text(
+                    text = "Envio ${shipment.number}",
+                    fontSize = 25.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(text = "Comprador: ${shipment.customerName}", fontWeight = FontWeight.SemiBold)
+                Text(text = "Total de productos: ${shipment.products.size}", fontWeight = FontWeight.SemiBold)
+            }
+        }
         LazyColumn(modifier = Modifier
-            .padding(6.dp)) {
+            .padding(15.dp)) {
             items(shipment.products){
                 product ->
                 ProductItem(viewModel,product)
+                Spacer(modifier = Modifier.height(5.dp))
             }
         }
+        Spacer(modifier = Modifier
+            .height(10.dp))
+
+        Row {
+            Button(onClick = {}){
+                Text("Siguiente")
+            }
+            Button(onClick = {}){
+                Text("Scan")
+            }
+        }
+
 
     }
 }
 
 @Composable
 fun ProductItem(viewModel:ShipmentViewModel,product:Product){
-    Card (modifier = Modifier
+    ElevatedCard(modifier = Modifier
         .fillMaxSize()
-        .padding(6.dp)){
-        Column {
+        .padding(2.dp)){
+        Column(modifier = Modifier
+            .padding(15.dp)){
             Text(product.name)
+            Spacer(modifier = Modifier
+                .height(10.dp))
             Row {
                 Text("x${product.quantity}")
-                Spacer(modifier = Modifier.width(20.dp))
-                Text("Cargado: ${viewModel.getLoadedQuantityProduct(product.id)}")
+                Spacer(modifier = Modifier.width(25.dp)
+                    .weight(2f))
+                Box(contentAlignment = Alignment.CenterEnd,
+                    modifier = Modifier
+                        .background(color = Color.White)){
+                    Text(text="Cargado: ${viewModel.getLoadedQuantityProduct(product.id)}")
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun vistaProd(){
+    ElevatedCard(modifier = Modifier
+        .fillMaxWidth()
+        .padding(2.dp)){
+        Column (modifier = Modifier
+            .padding(15.dp)){
+            Text("Pendrive 32gb kingston")
+            Spacer(modifier = Modifier
+                .height(10.dp))
+            Row {
+                Text("x5")
+                Spacer(modifier = Modifier.width(25.dp)
+                    .weight(2f))
+                Box(contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .background(color = Color.White)){
+                    Text(text="Cargado: 0")
+                }
+
 
             }
         }
-
-
     }
-
-
 }
