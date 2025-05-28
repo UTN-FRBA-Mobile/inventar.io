45bfbafd60c43a24e1e8712071c5ae981a76f741
Lucas Borrazás <lucas.borrazas@gmail.com>
2025-05-28 10:41:28 -0300

    Creación de componente EmptyResultsMessage

---
+++ InventarioApp/app/src/main/java/ar/edu/utn/frba/inventario/components/EmptyResultsMessage.kt
@@ -0,0 +1,78 @@
+package ar.edu.utn.frba.inventario.components
+
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import ar.edu.utn.frba.inventario.R
+
+
+@Composable
+fun EmptyResultsMessage(
+    message: String,
+    modifier: Modifier = Modifier
+) {
+    val iconIdByTheme = if (isSystemInDarkTheme()) {
+        R.drawable.empty_results_dark
+    } else {
+        R.drawable.empty_results_light
+    }
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.offset(y = (-48).dp)
+        ) {
+            Image(
+                painter = painterResource(id = iconIdByTheme),
+                contentDescription = null,
+                modifier = Modifier.size(80.dp),
+                contentScale = ContentScale.Fit
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Card(
+                modifier = Modifier.padding(horizontal = 32.dp),
+                shape = RoundedCornerShape(12.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceVariant.copy(alpha = 0.9f)
+                ),
+                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+            ) {
+                Text(
+                    text = message,
+                    style = MaterialTheme.typography.titleMedium ,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    fontWeight = FontWeight.SemiBold,
+                    modifier = Modifier.padding(16.dp),
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
+}
--- InventarioApp/app/src/main/java/ar/edu/utn/frba/inventario/screens/HomeScreen.kt
+++ InventarioApp/app/src/main/java/ar/edu/utn/frba/inventario/screens/HomeScreen.kt
@@ -13,14 +13,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
+import ar.edu.utn.frba.inventario.R
 import ar.edu.utn.frba.inventario.api.model.item.ItemStatus
 import ar.edu.utn.frba.inventario.api.model.shipment.Shipment
 import ar.edu.utn.frba.inventario.components.BranchLocationBar
 import ar.edu.utn.frba.inventario.components.CardItem
+import ar.edu.utn.frba.inventario.components.EmptyResultsMessage
 import ar.edu.utn.frba.inventario.components.StatusFilter
 import ar.edu.utn.frba.inventario.viewmodels.HomeViewModel
 
@@ -75,12 +78,17 @@ fun HomeBodyContent(
             style = MaterialTheme.typography.titleLarge,
             modifier = Modifier.padding(16.dp)
         )
-
-        LazyColumn(
-            modifier = Modifier.weight(1f)
-        ) {
-            itemsIndexed(shipments) { _, shipment ->
-                CardItem(item = shipment)
+        if (shipments.isEmpty()) {
+            EmptyResultsMessage(
+                message = stringResource(R.string.no_results_for_filters),
+                modifier = Modifier.weight(1f))
+        } else {
+            LazyColumn(
+                modifier = Modifier.weight(1f)
+            ) {
+                itemsIndexed(shipments) { _, shipment ->
+                    CardItem(item = shipment)
+                }
             }
         }
     }
