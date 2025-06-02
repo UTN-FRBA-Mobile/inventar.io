42dfca9653e4b1561e125a07359bfe952deafab3
Santiago Feijoo <santiago.feijoo96@gmail.com>
2025-06-02 19:37:42 -0300

    saque lo viejo de fer y deje el boton logout arriba, falta ver donde chota lo acomodo

--- InventarioApp/app/src/main/java/ar/edu/utn/frba/inventario/screens/UserScreen.kt
+++ InventarioApp/app/src/main/java/ar/edu/utn/frba/inventario/screens/UserScreen.kt
@@ -1,14 +1,16 @@
 package ar.edu.utn.frba.inventario.screens
 
+import android.R
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -16,6 +18,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -33,13 +36,10 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import ar.edu.utn.frba.inventario.utils.Base64Image
 import ar.edu.utn.frba.inventario.utils.Screen
 import ar.edu.utn.frba.inventario.viewmodels.LocationViewModel
@@ -79,21 +79,11 @@ fun UserBodyContent(
         userScreenViewModel.getUser()
     }
 
-    Text(
-        text = "Perfil",
-        style = MaterialTheme.typography.titleLarge,
-        modifier = Modifier.padding(16.dp)
-    )
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text("User")
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { userScreenViewModel.getUser() }) {
-            Text("Test get user")
-        }
+
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(MaterialTheme.colorScheme.secondaryContainer)
+    ){
         Button(onClick = {
             userScreenViewModel.doLogout()
             navController.navigate(Screen.Login.route) {
@@ -103,20 +93,15 @@ fun UserBodyContent(
         }) {
             Text("Logout")
         }
-
-        Text("parte de santi")
-
-
         if (user == null) {
             CircularProgressIndicator()
         } else {
             Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(50.dp),
+                    .padding(10.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-
                 Row (
                     modifier = Modifier
                         .fillMaxWidth()
@@ -127,26 +112,28 @@ fun UserBodyContent(
                     Base64Image(base64String = user!!.base64image, modifier = Modifier
                         .size(100.dp)
                         .clip(CircleShape)                    // Hace la imagen circular
-                        .border(2.dp, Color.Gray, CircleShape) // Opcional: borde gris redondeado
+                        .border(2.dp, Color.Gray, CircleShape)
                         .shadow(4.dp, CircleShape) )
                     Spacer(modifier = Modifier.width(16.dp))
-
                     Column {
                         Text(text = user!!.name, fontSize = 24.sp, fontWeight = FontWeight.Bold)
                         Spacer(modifier = Modifier.width(16.dp))
                         Text(
                             text = "@${user!!.username}",
                             fontSize = 16.sp,
-                            color = Color.Black
+                            color = MaterialTheme.colorScheme.primary
                         )
                     }
                     Log.d("Show base64", "Success: ${user!!.base64image}")
                 }
-
+                Text(
+                    text = "Ubicaciones Autorizadas",
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.primary
+                )
                 LazyColumn(modifier = Modifier.padding(8.dp)) {
                     items(testLocations.size) { index ->
                         var address by remember { mutableStateOf<String?>(null) }
-
                         LaunchedEffect(testLocations[index].latitude, testLocations[index].longitude) {
                             address = locationViewModel.getAddressFromLocation(context, testLocations[index].latitude, testLocations[index].longitude)
                         }
@@ -154,11 +141,14 @@ fun UserBodyContent(
                             .fillMaxWidth()
                             .padding(vertical = 8.dp)) {
                             Card (
-                                modifier = Modifier.fillMaxWidth()
+                                modifier = Modifier.fillMaxWidth(),
+                                colors = CardDefaults.cardColors(
+                                    containerColor = MaterialTheme.colorScheme.surface
+                                )
                             ){
-                                Text(text = "Nombre: ${testLocations[index].name}", fontSize = 18.sp, modifier = Modifier.padding(10.dp))
+                                Text(text = testLocations[index].name, fontSize = 18.sp, modifier = Modifier.padding(10.dp))
                                 Text(
-                                    text = "Dirección: ${address ?: "Cargando..."}",
+                                    text = address ?: "Cargando...",
                                     fontSize = 14.sp,
                                     modifier = Modifier.padding(10.dp)
                                 )
