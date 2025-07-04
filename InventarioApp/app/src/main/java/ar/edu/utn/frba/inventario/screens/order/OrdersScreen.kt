919734b759dbb088b06d05c45dfc4b8a1638e391
Lucas Borrazás <lucas.borrazas@gmail.com>
2025-07-03 23:39:32 -0300

    Se unifican estilos de botones en ShipmentDetailScreen y OrdersScreen

--- InventarioApp/app/src/main/java/ar/edu/utn/frba/inventario/screens/order/OrdersScreen.kt
+++ InventarioApp/app/src/main/java/ar/edu/utn/frba/inventario/screens/order/OrdersScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -20,6 +21,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -116,13 +118,15 @@ fun OrderBodyContent(
                 shape = RoundedCornerShape(50),
                 modifier = Modifier
                     .align(Alignment.BottomCenter)
-                    .padding(16.dp)
-                    .height(40.dp),
+                    .padding(18.dp)
+                    .width(180.dp)
+                    .height(50.dp),
                 containerColor = MaterialTheme.colorScheme.primary
             ) {
                 Text(
                     text = stringResource(R.string.scan),
-                    style = MaterialTheme.typography.labelLarge,
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.Bold,
                     modifier = Modifier.padding(horizontal = 15.dp, vertical = 0.dp)
                 )
             }
