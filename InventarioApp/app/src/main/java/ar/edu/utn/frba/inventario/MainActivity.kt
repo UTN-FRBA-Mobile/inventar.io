3edfea532fdda4f6416bf633babb274882506fc2
Nachiten [Desktop] <ibaptista@frba.utn.edu.ar>
2025-07-05 11:55:06 -0300

    Fix en BackStackLog

--- InventarioApp/app/src/main/java/ar/edu/utn/frba/inventario/MainActivity.kt
+++ InventarioApp/app/src/main/java/ar/edu/utn/frba/inventario/MainActivity.kt
@@ -1,14 +1,18 @@
 package ar.edu.utn.frba.inventario
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import ar.edu.utn.frba.inventario.composables.navigation.AppNavBar
@@ -29,6 +33,10 @@ class MainActivity : ComponentActivity() {
                 val currentBackStack by navController.currentBackStackEntryAsState()
                 val currentRoute = currentBackStack?.destination?.route
 
+                LaunchedEffect(Unit) {
+                    addPrintBackStackListener(navController)
+                }
+
                 val navBarItems: List<Screen> = listOf(
                     Screen.Shipments,
                     Screen.Orders,
@@ -48,5 +56,27 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+fun addPrintBackStackListener(navController: NavController) {
+    navController.addOnDestinationChangedListener { controller, _, _ ->
+        @SuppressLint("RestrictedApi")
+        val routes =
+            controller.currentBackStack.value.joinToString(", ") {
+                val route = it.destination.route
+                // Print all routes without query params
+                route?.substringBefore("?") ?: "null"
+            }
+
+        Log.d("BackStackLog", "BackStack: $routes")
+
+        // Print current route without query params
+        val currentRoute =
+            controller.currentBackStackEntry
+                ?.destination
+                ?.route
+                ?.substringBefore("?")
+        Log.d("BackStackLog", "Current Route: $currentRoute")
     }
 }
